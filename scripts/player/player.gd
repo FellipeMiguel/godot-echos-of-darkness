9c46c6fb06dcaf3a46d1373847baaa7e021a06b9
FellipeMiguel <fellioemiguel@gmail.com>
2024-06-10 21:20:23 -0300

    feat: attack animation and player's hitbox

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -7,6 +7,8 @@ var velocity: Vector2
 
 var jump_count: int = 0
 
+var attacking: bool = false
+
 export(int) var speed
 export(int) var jump_speed
 
@@ -15,6 +17,7 @@ export(int) var player_gravity
 func _physics_process(delta: float):
 	horizontal_movement_env()
 	vertical_moviment_env()
+	actions_env()
 	
 	gravity(delta)
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -22,16 +25,29 @@ func _physics_process(delta: float):
 	
 func horizontal_movement_env() -> void:
 	var input_direction: float = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if attacking:
+		velocity.x = 0
+		return
 	velocity.x = input_direction * speed
 
 func vertical_moviment_env() -> void:
 	if is_on_floor():
 		jump_count = 0
 		
-	if Input.is_action_just_pressed("ui_up") and jump_count < 2:
+	var jump_condition: bool = not attacking
+	if Input.is_action_just_pressed("ui_up") and jump_count < 2 and jump_condition:
 		jump_count += 1
 		velocity.y = jump_speed
 		
+func actions_env() -> void:
+	attack()
+	
+func attack() -> void:
+	var attack_condition: bool = not attacking
+	if Input.is_action_just_pressed("attack") and attack_condition and is_on_floor():
+		attacking = true
+		player_sprite.normal_attack = true
+	
 func gravity(delta: float) -> void:
 	velocity.y += delta * player_gravity
 	if velocity.y >= player_gravity:
--- scripts/player/player_texture.gd
+++ scripts/player/player_texture.gd
@@ -1,12 +1,17 @@
 extends Sprite
 class_name PlayerTexture
 
+var normal_attack: bool = false
+var suffix: String = "_right"
+
 export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
 export(NodePath) onready var player = get_node(player) as KinematicBody2D
 
 func animate(direction: Vector2) -> void:
 	verify_position(direction)
-	if direction.y != 0:
+	if player.attacking:
+		action_behavior()
+	elif direction.y != 0:
 		vertical_behavior(direction)
 	else:	
 		horizontal_behavior(direction)
@@ -14,8 +19,14 @@ func animate(direction: Vector2) -> void:
 func verify_position(direction: Vector2):
 	if direction.x > 0:
 		flip_h = false
+		suffix = "_right"
 	elif direction.x < 0:
 		flip_h = true
+		suffix = "_left"
+
+func action_behavior() -> void:
+	if player.attacking and normal_attack:
+		animation.play("attack" + suffix)
 
 func horizontal_behavior(direction: Vector2) -> void:
 	if direction.x != 0:
@@ -28,3 +39,14 @@ func vertical_behavior(direction: Vector2) -> void:
 		animation.play("fall")
 	elif direction.y < 0:
 		animation.play("jump")
+
+
+func on_animation_finished(anim_name):
+	match anim_name:
+		"attack_left":
+			normal_attack = false
+			player.attacking = false
+			
+		"attack_right":
+			normal_attack = false
+			player.attacking = false
